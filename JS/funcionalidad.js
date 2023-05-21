57e52e371a2ca82f8ac40ad4c46a537234801619
JulianSilva-coder <jsilvabe@unbosque.edu.co>
2023-05-20 22:53:13 -0500

    inicio

---
+++ JS/funcionalidad.js
@@ -0,0 +1,11 @@
+function registrar(){
+    location.href = "register.html";
+};
+
+function amazonas(){
+    alert("Funciona  Amazonas");
+};
+
+function antioquia(){
+    alert("Funciona  Antioquia");
+};
